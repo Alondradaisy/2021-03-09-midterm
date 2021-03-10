d77198520cd9e6ef79543d019914d0660631c4d8
Alondra Warren <daisy.codeimmersives.com>
2021-03-10 11:59:19 -0800

    cycling through blanking

--- main.js
+++ main.js
@@ -2,6 +2,60 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function ageToAbilities(number) {
+    let num = getInput();
+    for(let num of getInput <= 16) {
+        return "You can't drive.";
+
+    if(let num of getInput === 16 <= 17) 
+        return "You can drive but not vote.";
+    
+    }else{ if (let num of getInput === 18 <= 24);
+        return "You can vote, but not rent a car.";
+    
+    }else{ if (let num of getInput >= 25);
+        return "You can do pretty much anything."; 
+    }
+    { return age;
+}
+
+
+function oddIndices(arr) {
+    let odd = [];
+    for (let i = 0; i < arr.length; i += 2) {
+        if (odd[i] !== odd[0]) {
+        arr.filter(arr[i]);
+    }
+    }
+    return odd;
+}
+
+function numOddValues(arr) {
+    for (const oddValues of numOddValues) {
+        if (oddValues % 2 !== 0) {
+            return oddValues;
+        }
+    }
+}
+
+
+function averageStringLength(arr) {
+    let sumOfString = 0;
+    for (let i = 0; i <arr.length; i++);
+    sumOfString +=arr[i].length;
+    
+}
+
+
+function firstPunctuationIndex(arr) {
+    if arr[i]
+}
+
+
+function getPlace("highest", "lowest") {
+    if highest 
+}
+
 
 
 // ┌─────────────────────────────────────┐
